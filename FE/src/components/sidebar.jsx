7c6de9fb5d009e26e06c700d7a138f3c07505ae8
DishanGimhan24 <it22108340@my.sliit.lk>
2025-01-26 18:54:49 +0530

    0

--- FE/src/components/sidebar.jsx
+++ FE/src/components/sidebar.jsx
@@ -47,14 +47,14 @@ const Sidebar = () => {
 
   return (
     <div id="body-pd">
-      <header className="header" id="header">
+     {/*  <header className="header" id="header">
         <div className="header_toggle">
           <i className="bx bx-menu" id="header-toggle"></i>
         </div>
         <div className="header_img">
           <img src="https://i.imgur.com/hczKIze.jpg" alt="" />
         </div>
-      </header>
+      </header> */}
       <div className="l-navbar" id="nav-bar">
         <nav className="nav">
           <div>
@@ -63,7 +63,7 @@ const Sidebar = () => {
               <span className="nav_logo-name">Learning System</span>
             </a>
             <div className="nav_list">
-              <a href="" className="nav_link active">
+              <a href="/admin" className="nav_link active">
                 <i className="bx bx-grid-alt nav_icon"></i>
                 <span className="nav_name">Admin</span>
               </a>
